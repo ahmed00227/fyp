39b6c674a76bce670d7732f0cb6639e5fd0a8138
Awais Qureshi <awaisqureshi000006@gmail.com>
2025-03-21 20:53:31 +0500

    done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -66,10 +66,37 @@
     <div class="row">
     <div class="col-md-12">
       <header>
+        <div class="col-md-4">
             <div class="logo">
             <img src="images/images-removebg-preview.png" alt="" height="70px" width="70px">
             <h2><span style="color:darkblue;" >MEDI</span><span  style="color:rgb(235, 168, 0);" >FAST</span></h2>
             </div>
+            </div>
+            <!-- <div class="menus">
+                <nav>
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">About</a></li>
+                        <li><a href="">Chatbot</a></li>
+                        <li><a href="">Contact us</a></li>
+                    </ul>
+                </nav>
+            </div> -->
+            <div class="col-md-8">
+        <div class="search-sec">
+            <div class="search-bar">
+          
+                <input type="text" id="search" placeholder="Search for products">
+            </div>
+            <div class="login-btn">
+            <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+            <button class="btn ">Login</button>
+            </div>
+          
+        </div>
+        </div>
+        </div>
+        <div class="col-md-12">
             <div class="menus">
                 <nav>
                     <ul>
@@ -80,17 +107,6 @@
                     </ul>
                 </nav>
             </div>
-            
-        <div class="search-sec">
-            <div class="search-bar">
-          
-                <input type="text" id="search" placeholder="Search for products"><i class="fa fa-shopping-cart" aria-hidden="true"></i>
-            </div>
-            <div class="login-btn">
-            <button class="btn ">Login</button>
-            </div>
-          
-        </div>
         </div>
 
       </header>
@@ -107,7 +123,7 @@
          <div class="high-stan">
             
          <h1>Welcome to our <span style="color:darkblue;">MEDI</span><span style="color:orange;">FAST</span></h1>
-         <h4> The store aims to create a seamlessshopping experience  <br>for its customers, with high-quality products <br>and expert support readily available online.</h4>
+         <h4> The store aims to create a seamless shopping experience for its customers, with high-quality products <br>and expert support readily available online.</h4>
          </div>
          <div class="header-btn">
            <button class="btn " id="shop-now">Shop Now</button>
@@ -122,17 +138,17 @@
  <div class="service">
  <div class="container">
     <div class="row">
-        <div class="col-md-4">
+        <div class="col-lg-4 col-md-6">
         <i class="fas fa-shipping-fast"></i>
             <h2>fast delivery</h2>
             <p>Medifast offers fast and reliable delivery, ensuring your medical supplies and healthcare products reach you quickly and securely, right at your doorstep.</p>
             </div>
-            <div class="col-md-4">
+            <div class="col-md-6 col-lg-4">
             <i class="fa-solid fa-file-contract"></i>
             <h2>license of government</h2>
             <p>Medifast is a health and wellness company that follows government rules. In 2012, it paid a fine for false weight-loss claims. It follows laws to keep its business fair and honest.</p>
             </div>
-            <div class="col-md-4">
+            <div class="col-md-6 col-lg-4">
             <i class="fa-solid fa-user-tie"></i>
             <h2>support 24/7</h2>
             <p>It ensures that customers can get help anytime, day or night, for their queries, orders, or any support they need.</p>
@@ -161,7 +177,7 @@
 <div class="container">
     <div class="row">
         
-            <div class="col-md-4">
+            <div class="col-md-6 col-lg-4">
                <div class="card">
                 <img src="images/dental.jpg" alt="">
                 <div class="overlay">
@@ -171,7 +187,7 @@
                 </div>
                </div>
             </div>
-            <div class="col-md-4">
+            <div class="col-lg-4 col-md-6">
                <div class="card">
                 <img src="images/skin-care.jpg" alt="">
                 <div class="overlay">
@@ -182,7 +198,7 @@
                 
                </div>
             </div>
-            <div class="col-md-4">
+            <div class="col-md-6 col-lg-4">
                <div class="card">
                 <img src="images/vitamin.jpg" alt="">
                 <div class="overlay">
