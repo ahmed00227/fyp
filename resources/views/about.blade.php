5319a45c7d68ce68de7d42bfda4798537a42de0b
Awais Qureshi <awaisqureshi000006@gmail.com>
2025-03-27 20:58:42 +0500

    done

---
+++ resources/views/about.blade.php
@@ -0,0 +1,92 @@
+
+<html>
+<link rel="stylesheet" href="css/bootstrap/CSS/bootstrap-4.0.0-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="css/styling.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" /> 
+     <title>Chatbot</title>
+    <body>
+        <div class="about">
+<div class="container">
+    <div class="row">
+      
+           <div class="col-md-6 about_sec">
+            <h1>About</h1>
+            <p>We are a trusted <b>online healthcare platform </b>dedicated to providing <b>safe, affordable, and high-quality medicines</b> at your doorstep. With a focus on convenience and patient well-being, we bridge the gap between healthcare providers and patients through seamless online <br>Beyond just being an online medicine provider, we strive to be a one-stop healthcare destination where people can find medical advice, wellness tips, and health products tailored to their needs. Whether you're looking for chronic disease medications, over-the-counter drugs, or wellness essentials, we are here to support your health journey with trust, transparency, and excellence.
+
+          </p>
+              </div>
+               <div class="col-md-6 about_sec">
+            <img src="../images/about-picaai.png" alt="" width=100%>
+              </div>
+              <div class="col-md-6 about_sec">
+           
+            <img src="../images/story-picaai.png" alt="" width=100% height="500px">
+
+           </p>
+              </div>
+               <div class="col-md-6 about_sec">
+               <h1>our story</h1>
+              <p>we recognized the need for <b>quick and hassle-free</b> access to medicines and healthcare essentials. With rising healthcare costs and limited accessibility, our goal was to <b>simplify online medicine ordering</b> and ensure that quality healthcare is just a click away. <br>Understanding these challenges, we built a <b>trusted, technology-driven healthcare platform</b> that provides a seamless experience—from browsing and ordering medicines to receiving doorstep delivery. Our online pharmacy is designed to eliminate the <b>barriers of distance, time, and availability</b> , ensuring that everyone has access to safe, authentic, and affordable medications anytime, anywhere 
+              </p>
+              </div>
+     <div class="container">
+     <div class="row trust_us">
+     <div class="col-md-12 ">
+              <h1>why trust us?</h1>
+              </div>
+              <div class="col-md-3 about_trust">
+               <i class="fa-solid fa-shield-halved"></i>
+               <h3>Safety First</h3>
+               <p>100% genuine medicines sourced from licensed suppliers </p>
+              </div>
+              <div class="col-md-3 about_trust">
+              <i class="fas fa-shipping-fast"></i>
+              <h3>Fast & Reliable Deliver</h3>
+               <p>  Express shipping for urgent medical needs </p>
+              </div>
+              <div class="col-md-3 about_trust">
+              <i class="fa-solid fa-credit-card"></i>
+              <h3>Affordable Price</h3>
+               <p>Competitive pricing with discounts and offers </p>
+              </div>
+              <div class="col-md-3 about_trust">
+              <i class="fa-solid fa-book-open-reader"></i>
+              <h3>Expert Guidance</h3>
+               <p> Certified pharmacists and healthcare professionals available for </p>
+              </div>
+            
+
+     </div>
+     </div>
+            
+           
+    </div>
+   
+</div>
+<div class="">
+
+
+<div class="container">
+    <div class="row">
+  <div class="col-md-12 commitment">
+    <!-- <h1>our commitment</h1> -->
+  </div>
+  <div class="col-md-6 commitment">
+  <h1>our commitment</h1>
+  <i class="fa fa-check" aria-hidden="true"></i> <span> Providing only <b>certified and FDA-approved</b>medicines</span><br>
+  <i class="fa-solid fa-lock"></i><span> Ensuring <b>secure and private</b> transactions </span><br>
+  <i class="fa-solid fa-heart"></i><span>Offering <b>expert medical consultation</b> for better decisions </span><br>
+  <i class="fa-solid fa-pills"></i><span> Delivering <b>medications to your doorstep with ease</b> </span>
+ 
+  </div>
+  <div class="col-md-6 commitment">
+  <img src="../images/commitment-picaai.png" alt="" width=100%>
+  </div>
+    </div>
+
+</div>
+</div>
+</div>
+  
+    </body>
+</html>
